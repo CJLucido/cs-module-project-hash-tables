f869521ed4983215787d13364c13da170fbc0e02
Carlo <c.jose.lucido@gmail.com>
2020-07-15 21:56:23 -0700

    untested no_dups problem, didn't use a hash table, not sure if I have to to make it O(n)

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,24 @@
 def no_dups(s):
     # Your code here
+    fStrings = []  
+    fWords = []
 
+    for fLine in s:
+        fStrings.append(fLine)
+
+    for fLineHere in fStrings:
+        for character in fLineHere:
+            if character.isalpha():
+                character.lower()
+
+        fLineHere.split() #flinehere now a list
+        for i in fLineHere:
+            if i in fWords:
+                pass
+            else:
+                fWords.append(i)
+
+    print(" ".join(fWords))
 
 
 if __name__ == "__main__":
